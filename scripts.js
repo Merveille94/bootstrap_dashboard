bce6f203f9c0490b47e1b04e850516f519bfcd28
Merveille94 <emanueleguehi@gmail.com>
2024-02-27 21:28:11 +0000

    Update

---
+++ scripts.js
@@ -0,0 +1,30 @@
+    // Data for the chart
+    const data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'monthly',
+            data: [50, 60, 70, 80, 90, 100, 110],
+            backgroundColor: 'rgb(240, 255, 144)', // Lemon green color
+            borderColor: 'rgba(54, 162, 235, 1)', // Border color
+            borderWidth: 1.9,
+            borderRadius: 50 // Pill shape
+        }]
+    };
+
+    // Configuration options
+    const options = {
+        scales: {
+            y: {
+                beginAtZero: true, // Start y-axis at 0
+            }
+        },
+        
+    };
+
+    // Initialize the chart
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
